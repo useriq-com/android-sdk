8f0d455944442ec0ea03ae2176a9dad2ddabc5a7
Palkesh Jain <palkesh.jain@useriq.com>
2018-09-18 18:21:04 +0530

    android: integrated sdk into the sample app

--- Sample app/app/src/main/java/com/useriq/android_sample/MainActivity.java
+++ Sample app/app/src/main/java/com/useriq/android_sample/MainActivity.java
@@ -3,12 +3,9 @@ package com.useriq.android_sample;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.useriq.sdk.UserIQSDK;
--- Sample app/app/src/main/java/com/useriq/android_sample/MyApplication.java
+++ Sample app/app/src/main/java/com/useriq/android_sample/MyApplication.java
@@ -4,12 +4,14 @@ import android.app.Application;
 
 import com.useriq.sdk.UserIQSDK;
 
+
 public class MyApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        UserIQSDK.init(this, "API_KEY");
+        UserIQSDK.init(this, "9ed2bc23e5c1cb4e852b82cba195a83cede6b5a9");
+        UserIQSDK.setHost("dev-mobile.useriq.com/sdk");
     }
 }
